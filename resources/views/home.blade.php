4c5886c49d6ae1cd71b9abe12cff542daf99d3ee
FilippoFoti <filippofoti7@gmail.com>
2023-05-24 14:35:20 +0200

    Add Cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <h2>ciao</h2>
+    <div class="container">
+        <div class="row row-cols-5">
+            @foreach ($movies as $movie)
+                <div class="col">
+                    <div class="card">
+                        <img src="{{ $movie->image }}" class="card-img-top" alt="Locandina">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <p class="card-text">{{ $movie->original_title }}</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Paese: {{ $movie->nationality}}</li>
+                            <li class="list-group-item">Data: {{ $movie->date}}</li>
+                            <li class="list-group-item">Voto: {{ $movie->vote}}</li>
+                        </ul>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
